7b4fd87ecb45f411562b9c72700e75af9b2219e5
Altenius <jacobjm18@gmail.com>
2019-07-27 21:08:42 -0400

    Added all checksum regions

--- checksum.py
+++ checksum.py
@@ -8,10 +8,55 @@ def sum_range(buffer, start, end):
         sum += struct.unpack_from(">H", data, i)[0]
     return sum
 
-def calculate(buffer):
-    sum = sum_range(buffer, 0, 0x4000) + sum_range(buffer, 0x20000, 0x7FFFD)
-    sum = sum % 2**16
-    return sum
+def calculate_ranges(buffer, ranges):
+    sum = 0
+    for range in ranges:
+        sum += sum_range(buffer, range[0], range[1])
+    return sum % 2**16
+
+regions = [
+    ("Main", 0x500, [(0, 0x4000), (0x20000, 0x7FFFD)]),
+    ("Engine Calibration", 0x8000, [(0x8000, 0x14000)]),
+    ("Engine Diagnostics", 0x14000, [(0x14000, 0x16E00)]),
+    ("Transmission Calibration", 0x16E00, [(0x16E00, 0x1BE00)]),
+    ("Transmission Diagnostics", 0x1BE00, [(0x1BE00, 0x1C800)]),
+    ("Fuel System", 0x1C800, [(0x1C800, 0x1E520)]),
+    ("Vehicle System", 0x1E520, [(0x1E520, 0x1EEA0)]),
+    ("VSS", 0x1EEA0, [(0x1EEA0, 0x1EFA0)])
+]
+
+def correct_region(data, region):
+    print("Correcting region \"" + region[0] + "\"")
+    # Zero checksum word
+    offset = region[1]
+    data[offset] = 0
+    data[offset + 1] = 0
+
+    sum = calculate_ranges(data, region[2])
+    # Convert to two's complement
+    sum = (0xFFFF ^ sum) + 1
+
+    packed = struct.pack(">H", sum)
+    data[offset] = packed[0]
+    data[offset + 1] = packed[1]
+
+    new_sum = calculate_ranges(data, region[2])
+    if new_sum != 0:
+        print("  Checksum correction failed, got " + hex(new_sum) + ", expected 0")
+        return False
+    else:
+        print("  Correction verified")
+        return True
+
+
+def correct(data):
+    """Corrects all regions"""
+    success = True
+    for region in regions:
+        if not correct_region(data, region):
+            success = False
+    return success
+
 
 if (__name__ == "__main__"):
     parser = argparse.ArgumentParser(description="Verify checksum of LS1 ROM")
@@ -26,30 +71,23 @@ if (__name__ == "__main__"):
     file.close()
 
     if args.verify:
-        sum = calculate(data)
-        if sum == 0:
-            print("Correct")
-        else:
-            print("Incorrect. Got checksum " + hex(sum))
+        for region in regions:
+            print("Checking region " + region[0])
+            sum = calculate_ranges(data, region[2])
+            if sum == 0:
+                print("Correct")
+            else:
+                print("Incorrect. Got checksum " + hex(sum))
+
     elif args.correct != None:
-        sum = sum_range(data, 0, 0x500) + sum_range(data, 0x502, 0x4000) + sum_range(data, 0x20000, 0x7FFFD)
-        sum = sum % 2**16
-
         data = bytearray(data)
-
-        # Convert to two's complement
-        sum = (0xFFFF ^ sum) + 1
-
-        packed = struct.pack(">H", sum)
-        data[0x500] = packed[0]
-        data[0x501] = packed[1]
-
-        new_sum = calculate(data)
-        if new_sum != 0:
-            print("Checksum correction failed, got " + hex(new_sum) + ", expected 0")
+        if not correct(data):
+            print("WARNING: Failed to correct at least one region")
         else:
-            args.correct.write(data)
-            args.correct.close()
-            print("Corrected checksum")
+            print("All checksums corrected and verified")
+
+        args.correct.write(data)
+        args.correct.close()
+        
     else:
         parser.print_help()
